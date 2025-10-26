1fbb8a88d5430719cb3205dedc89259fa49c5760
Hanulus <chingizuraimov@gmail.com>
2025-10-26 22:03:08 +0500

    Update __init__.py

---
+++ strategies/__init__.py
@@ -0,0 +1,15 @@
+
+#Strategies package for delivery system.
+#Contains all delivery strategy implementations.
+
+from .delivery_strategy import DeliveryStrategy
+from .standard_delivery import StandardDelivery
+from .express_delivery import ExpressDelivery
+from .pickup_delivery import PickupDelivery
+
+__all__ = [
+    'DeliveryStrategy',
+    'StandardDelivery',
+    'ExpressDelivery',
+    'PickupDelivery'
+]
